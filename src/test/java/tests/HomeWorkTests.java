9073c8576518717e2576e16081bd3241ce33769e
alexangl <alexangl@mail.ru>
2024-12-12 21:52:05 +0300

    HomeWorkTests added final

--- src/test/java/tests/HomeWorkTests.java
+++ src/test/java/tests/HomeWorkTests.java
@@ -5,16 +5,17 @@ import com.codeborne.selenide.Configuration;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
+/* import static com.codeborne.selenide.Condition.text; */
 import static com.codeborne.selenide.Condition.text;
-import static com.codeborne.selenide.Selenide.$;
-import static com.codeborne.selenide.Selenide.open;
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selenide.*;
 
 public class HomeWorkTests {
 
     @BeforeAll
     static void beforeAll() {
         Configuration.browserSize = "1920x1080";
-        Configuration.baseUrl = "https://demoqa.com";
+        Configuration.baseUrl = " https://demoqa.com";
         Configuration.pageLoadStrategy = "eager";
         Configuration.holdBrowserOpen = true;
         Configuration.timeout = 5000; // default 4000
@@ -22,16 +23,35 @@ public class HomeWorkTests {
 
     @Test
     void fillFormTest() {
-        open("/text-box");
-        $("#userName").setValue("Alex");
-        $("#userEmail").setValue("alex@egorov.com");
-        $("#currentAddress").setValue("Some street 1");
-        $("#permanentAddress").setValue("Another street 1");
+        open("/automation-practice-form");
+        $("#firstName").setValue("Maria");
+        $("#lastName").setValue("Petrova");
+        $("#userEmail").setValue("mp@gmail.com");
+        $("#genterWrapper").$(byText("Female")).click();
+        $("#userNumber").setValue("9550012345");
+        $("#dateOfBirthInput").click();
+        $(".react-datepicker__month-select").click();
+        $(".react-datepicker__month-select").selectOption("December");
+        $(".react-datepicker__year-select").selectOption("1985");
+        $$(".react-datepicker__day:not(.react-datepicker__day--outside-month)").findBy(text("31")).click();
+        $("#subjectsInput").setValue("Literature").pressEnter();
+        $("#hobbiesWrapper").$(byText("Music")).click();
+        $("#uploadPicture").uploadFromClasspath("my_photo.png");
+        $("#currentAddress").setValue("Russia");
+        $("#react-select-3-input").setValue("Haryana").pressEnter();
+        $("#react-select-4-input").setValue("Karnal").pressEnter();
         $("#submit").click();
 
-        $("#output #name").shouldHave(text("Alex"));
-        $("#output #email").shouldHave(text("alex@egorov.com"));
-        $("#output #currentAddress").shouldHave(text("Some street 1"));
-        $("#output #permanentAddress").shouldHave(text("Another street 1"));
+        $("#example-modal-sizes-title-lg").shouldHave(text("Thanks for submitting the form"));
+        $(".table").$(byText("Student Name")).sibling(0).shouldHave(text("Maria Petrova"));
+        $(".table").$(byText("Student Email")).sibling(0).shouldHave(text("mp@gmail.com"));
+        $(".table").$(byText("Gender")).sibling(0).shouldHave(text("Female"));
+        $(".table").$(byText("Mobile")).sibling(0).shouldHave(text("9550012345"));
+        $(".table").$(byText("Date of Birth")).sibling(0).shouldHave(text("31 December,1985"));
+        $(".table").$(byText("Subjects")).sibling(0).shouldHave(text("Literature"));
+        $(".table").$(byText("Hobbies")).sibling(0).shouldHave(text("Music"));
+        $(".table").$(byText("Picture")).sibling(0).shouldHave(text("my_photo.png"));
+        $(".table").$(byText("Address")).sibling(0).shouldHave(text("Russia"));
+        $(".table").$(byText("State and City")).sibling(0).shouldHave(text("Haryana Karnal"));
     }
 }
